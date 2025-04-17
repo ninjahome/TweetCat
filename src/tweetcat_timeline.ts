1911aad5e4321c950787e069f1b6867e454978f8
basdevelop <hyperorchid@outlook.com>
2025-04-17 09:44:44 +0800

    setup style of tweetCatLine

--- src/tweetcat_timeline.ts
+++ src/tweetcat_timeline.ts
@@ -1,6 +1,8 @@
 import {observeForElement} from "./utils";
 import {parseContentHtml} from "./content";
 
+const itemSelClasses = ['r-1kihuf0', 'r-sdzlij', 'r-1p0dtai', 'r-hdaws3', 'r-s8bhmr', 'r-u8s1d', 'r-13qz1uu']
+
 export async function appendCatPresent() {
     observeForElement(document.body, 3000, () => {
         const navDiv = document.querySelector('nav[aria-live="polite"][role="navigation"]');
@@ -13,16 +15,49 @@ export async function appendCatPresent() {
             console.warn("------>>> failed to find top menu [for you] [following]")
             return;
         }
-
-        console.log("------>>>nav div", menuList);
-
         const contentTemplate = await parseContentHtml('html/content.html');
-        const tweetCatDiv = contentTemplate.content.getElementById("tweetCat-present-top")!;
+        const clone = contentTemplate.content.getElementById("tweetCat-present-top")!.cloneNode(true) as HTMLElement;
+
+        menuList.querySelectorAll("a").forEach(linkElm => {
+            linkElm.addEventListener('click', () => {
+                itemSelClasses.forEach(className => {
+                    clone.classList.remove(className)
+                })
+            })
+        });
+
         if (menuList.children.length === 2) {
-            menuList.insertBefore(tweetCatDiv, menuList.lastChild)
+            menuList.append(clone)
         } else {
-            menuList.insertBefore(tweetCatDiv, menuList.children[2])
+            menuList.insertBefore(clone, menuList.children[2])
+        }
+
+        clone.onclick = (e) => {
+            e.preventDefault();
+            pullTweetCatContent(menuList as HTMLElement, clone)
         }
 
     }, false);
+}
+
+function pullTweetCatContent(menuList: HTMLElement, selectedItem: HTMLElement) {
+
+    console.log("------>>>tweetCat content");
+    menuList.querySelectorAll("a").forEach(linkElm => {
+        linkElm.setAttribute('aria-selected', 'false');
+        linkElm.setAttribute('tabindex', '-1');
+        const subLineItem = linkElm.querySelector(".css-175oi2r.r-xoduu5") as HTMLElement
+        itemSelClasses.forEach(className => {
+            subLineItem.classList.remove(className)
+        })
+        subLineItem.removeAttribute('style');
+    })
+
+    selectedItem.setAttribute('aria-selected', 'true');
+    selectedItem.removeAttribute('tabindex');
+    const subLineItem = selectedItem.querySelector(".css-175oi2r.r-xoduu5") as HTMLElement
+    itemSelClasses.forEach(className => {
+        subLineItem.classList.add(className)
+    })
+    subLineItem.setAttribute('style', 'background-color: rgb(29, 155, 240)');
 }
