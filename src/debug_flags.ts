8662cdc1c79c48cf4695329202865613e5aa2884
basdevelop <hyperorchid@outlook.com>
2025-07-17 12:20:15 +0800

    start point of a big refactoring action

---
+++ src/debug_flags.ts
@@ -0,0 +1,8 @@
+// debug_flags.ts
+export const DBG = {
+    ANCHOR: true,   // 风险1相关
+    MOUNT:  true,   // 风险2相关
+    DIFF:   true,   // 风险3相关
+    VERBOSE_SCROLL: false, // 如需
+    ENTRY_PAGER:true,
+};
